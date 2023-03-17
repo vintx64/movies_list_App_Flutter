9805bda9912055bded4a30cb7d618d5d0e63c1bd
vintx64 <vintx64@gmail.com>
2023-03-17 23:39:25 +0200

    create bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:movies_list_task/cubit/movie_cubit.dart';
+import 'package:movies_list_task/simple_bloc_observer.dart';
 import 'package:movies_list_task/views/home_view.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MoviesList());
 }
 
@@ -12,8 +16,11 @@ class MoviesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeView(),
+    return BlocProvider(
+      create: (context) => MovieCubit(),
+      child: const MaterialApp(
+        home: HomeView(),
+      ),
     );
   }
 }
